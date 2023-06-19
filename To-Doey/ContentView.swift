9ca3ec185a24d31c5b972a9c9f2ca3e07a48b6fd
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2023-06-19 12:47:35 -0400

    Initial Commit

---
+++ To-Doey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-Doey
+//
+//  Created by Deonte Kilgore on 6/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-Doey/To_DoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoeyApp.swift
+//  To-Doey
+//
+//  Created by Deonte Kilgore on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct To_DoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
